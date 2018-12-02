9d473f6e821513332eb3623c54c790cac9f5cd0c
Mona Lisa <Vladibui@mail.ru>
2018-12-02 17:41:34 +0300

    kak-to tak

---  kvadratura_dura.py
+++  kvadratura_dura.py
@@ -1,3 +1,14 @@
+from math import *
 print ("vvedite vse koeff")
 a,b,c=list(map(float, input().split()))
-print(a,b,c)
+discr=b*b-4*a*c
+if discr<0:
+    print("korni potracheni")
+elif discr==0:
+    x=-b/(2*a)
+    print("odin koren",x)
+else:
+    x1=(-b+sqrt(discr)/(2*a))
+    print("odin koren",x1)
+    x2=(-b-sqrt(discr)/(2*a))
+    print("vtoroy koren",x2)
